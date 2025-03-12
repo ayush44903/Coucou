e7843adb74d57fbaf3f5cac87d1573e4aea84ade
aprankhunger <khungerapran@gmail.com>
2025-03-12 18:27:00 +0530

    Updated for Railway deployment

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: ["https://socketexample-production.up.railway.app", "http://localhost:3000"],
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
 // Serve static files from public directory
 app.use(express.static('public'));
