af559eeb38fb50439a5ffeac8ea9324fa3b5866e
aprankhunger <khungerapran@gmail.com>
2025-03-12 18:04:05 +0530

    Fixes

--- server.js
+++ server.js
@@ -74,5 +74,6 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
