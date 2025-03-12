cb81071615ff6b400c615a495bf6296b147c10f7
aprankhunger <khungerapran@gmail.com>
2025-03-12 18:07:32 +0530

    Back to basic

--- server.js
+++ server.js
@@ -74,6 +74,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    console.log(`Server running on port ${PORT}`);
 });
